a598fde7d22e6a0ec14f2a6de30924eb3d794f7a
Ben Carpenter <bacarpenter04@gmail.com>
2022-04-08 13:37:30 -0400

    Write entry class file

---
+++ entry.py
@@ -0,0 +1,8 @@
+class Entry:
+    def __init__(self, id, title, text, timestamp):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.timestamp = timestamp
+
+    
