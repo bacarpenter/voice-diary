b005986abc2e869b76472c61e0cca52d93a908d8
Ben Carpenter <bacarpenter04@gmail.com>
2022-04-08 13:40:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@
 # Ben Carpenter and Nancy Onyimah
 # April 7, 2022
 # ------------- app.py -------------
-
+import database
 
 def main():
+  database.initialize_connection()
   decision = input("Welcome! Do you want to write an entries or read an existing one?")
   
   if decision == "w" or "W":
@@ -18,17 +19,7 @@ def main():
   title = input ("What is the title of your entry?")
 
   text = input ("What do you want to write in your entry?")
-  pass
-
-def create_entry(text, title):
-  print(f"Entry created of title {title} and content {text}")
-
-def read_all_entries():
-  pass
-
-def read_entry(id):
-  pass
-
+  database.close_connection()
 
 if __name__ == "__main__":
   main()
