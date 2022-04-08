de3f1c452c044d66b8c74a591af32ab42e70ef36
Ben Carpenter <bacarpenter04@gmail.com>
2022-04-08 13:37:20 -0400

    Write database functions

--- database.py
+++ database.py
@@ -9,13 +9,18 @@
 # Connection initialization 
 import mysql.connector
 from mysql.connector import errorcode
+import datetime
+from entry import Entry
 
-global db
+db = None
 
 def initialize_connection():
+    global db
     """
     Start the Database connection
     """
+
+    # Code here based on dev example https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
     try:
         db = mysql.connector.connect(user='python_user',
                                     password='Python!',
@@ -32,23 +37,61 @@ def close_connection():
     """
     Close the database connection
     """
-    cnx.close()
+    db.close()
 
 
-def create_entry(text, title)-> int:
+def create_entry(title: str, text: str)-> int:
     """
     Create an entry in the database and return an int of it's ID
     """
-    pass
+    # Code here based on dev example https://dev.mysql.com/doc/connector-python/en/https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
+    cursor = db.cursor()
+
+    add_entry = ("INSERT INTO entries "
+                "(title, text, timestamp) "
+                "VALUES (%s, %s, %s)")
+
+    entry_data = (title, text, datetime.datetime.now())
+
+    cursor.execute(add_entry, entry_data)
+    entry_id = cursor.lastrowid
+
+    db.commit()
+
+    cursor.close()
+    return entry_id
 
 def read_all_entries():
     """
     Return all journal entries in the database
     """
-    pass
 
-def read_entry(id):
+    # Code here based on https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
+    cursor = db.cursor()
+
+    query = "SELECT * FROM entries"
+
+    cursor.execute(query)
+
+    entries = []
+    for entry in cursor:
+        entries.append(Entry(entry[0], entry[2], entry[1], entry[3]))
+
+    cursor.close()
+    return entries
+
+def read_entry(id: int):
     """
     Return the entry with given id
     """
-    pass
+    # Code here based on https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
+    cursor = db.cursor()
+    query = "SELECT * FROM entries WHERE id = %s"
+    cursor.execute(query, [id])
+
+    entries = []
+    for entry in cursor:
+        entries.append(entry)
+
+    entry = entries[0]
+    return Entry(entry[0], entry[2], entry[1], entry[3])
